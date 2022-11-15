472919c5b84b2a97db83d6f63092b17e7119d081
Om Kumar <35757329+om60@users.noreply.github.com>
2022-11-15 12:15:09 +0530

    Update streamlit_app.py
    added new breakfast and new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🍜 Omega 3 and & Blueberry Oatmeal')
 streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Tost')
+
+streamlit.header('🍌🥭Build Your Own Fruit smoothie🥝🍇')
